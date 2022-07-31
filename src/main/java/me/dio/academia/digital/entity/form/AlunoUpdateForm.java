36791d3e6c5c3c463857ffffc45bca405083c743
Lopes <ifes.lopes@gmail.com>
2022-07-31 03:54:58 -0300

    teste

--- src/main/java/me/dio/academia/digital/entity/form/AlunoUpdateForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/AlunoUpdateForm.java
@@ -2,9 +2,13 @@ package me.dio.academia.digital.entity.form;
 
 import java.time.LocalDate;
 
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
 public class AlunoUpdateForm {
 
-  private String nome;
+  private String nome1;
 
   private String bairro;
 
